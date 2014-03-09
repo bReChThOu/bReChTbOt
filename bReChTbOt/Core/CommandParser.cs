24bfad8148a6e46fc485595bbb068e605c9857f4
bReChThOu <brechthouben@outlook.com>
2014-03-09 12:08:47 +0100

    Removed unused usings.

--- bReChTbOt/Core/CommandParser.cs
+++ bReChTbOt/Core/CommandParser.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using bReChTbOt.Config;
+﻿using bReChTbOt.Config;
 using bReChTbOt.Map;
+using System;
+using System.Linq;
 
 namespace bReChTbOt.Core
 {
