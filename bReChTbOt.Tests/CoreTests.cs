abbf74e031ed74c955e4cb23fc4d637376dd4655
bReChThOu <brechthouben@outlook.com>
2014-03-10 22:14:57 +0100

    Added test-project

---
+++ bReChTbOt.Tests/CoreTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bReChTbOt;
+using bReChTbOt.Core;
+using bReChTbOt.Map;
+
+namespace bReChTbOt.Tests
+{
+	[TestClass]
+	public class CoreTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+		}
+	}
+}
--- bReChTbOt/Map/RegionStatus.cs
+++ bReChTbOt/Map/RegionStatus.cs
@@ -1,5 +1,13 @@
-﻿namespace bReChTbOt.Map
+﻿// <copyright file="RegionStatus.cs">
+//		Copyright (c) 2013 All Rights Reserved
+// </copyright>
+// <author>Brecht Houben</author>
+// <date>10/03/2014</date>
+namespace bReChTbOt.Map
 {
+	/// <summary>
+	/// Region Status enumeration
+	/// </summary>
     public enum RegionStatus
     {
         Initialized = 1,
