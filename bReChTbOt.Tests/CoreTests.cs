d9fe8542b472afee5cb039e9a6900d94ec741e56
Brecht Houben <brechthouben@outlook.com>
2014-03-20 15:07:34 +0100

    Added unit test

--- bReChTbOt.Tests/CoreTests.cs
+++ bReChTbOt.Tests/CoreTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using bReChTbOt;
 using bReChTbOt.Core;
 using bReChTbOt.Map;
+using System.Collections.Generic;
 
 namespace bReChTbOt.Tests
 {
@@ -12,7 +13,16 @@ namespace bReChTbOt.Tests
 		[TestMethod]
 		public void TestMethod1()
 		{
+            //Arrange
+            Region region = new Region() { ID = 1 };
+            SuperRegion superregion = new SuperRegion() { ID = 1 };
 
+            //Act
+            superregion.AddChildRegion(region);
+            int childcount = superregion.ChildRegions.Count;
+
+            //Assert
+            Assert.IsTrue(childcount == 1);
 		}
 	}
 }
