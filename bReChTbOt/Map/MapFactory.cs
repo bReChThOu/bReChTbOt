d884cdb0ea4c242773af687da962a57056138b9c
bReChThOu <brechthouben@outlook.com>
2014-05-03 17:21:01 +0200

    Bugfix. v94

--- bReChTbOt/Map/MapFactory.cs
+++ bReChTbOt/Map/MapFactory.cs
@@ -777,7 +777,7 @@ namespace bReChTbOt.Map
         {
             if (sourceRegion != null && targetRegion != null)
             {
-                if (sourceRegion.NbrOfArmies > 5)
+                if (sourceRegion.NbrOfArmies > 5 || (sourceRegion.Player.PlayerType == PlayerType.Me && targetRegion.Player.PlayerType == PlayerType.Me))
                 {
                     ArmyTransfer transfer = new ArmyTransfer() { SourceRegion = sourceRegion, TargetRegion = targetRegion, Armies = GetRequiredArmies(sourceRegion, targetRegion) };
                     transfers.Add(transfer);
